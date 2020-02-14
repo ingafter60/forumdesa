296509e78facf3c94f1d22d26f66b9d38d800b72
ing <inyoman_gurnitha@yahoo.com>
2020-02-15 02:49:22 +0700

    Part 6 - 7. Reusable Pagination Template

--- myproject/urls.py
+++ myproject/urls.py
@@ -74,8 +74,9 @@ urlpatterns = [
     # BORDS
     url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     # url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    # url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         views.PostUpdateView.as_view(), name='edit_post'),
@@ -83,3 +84,9 @@ urlpatterns = [
     # ADMIN
     url(r'^admin/', admin.site.urls),
 ]
+
+
+# ------
+
+
+
