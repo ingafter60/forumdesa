357ed43d1f23774fb021d1737cf3fb4da9ca5ae9
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 16:33:31 +0700

    Part 5 - 1. Protecting Views & Configuring Login Next Redirect

--- boards/views.py
+++ boards/views.py
@@ -73,8 +73,54 @@
 
 # PART 3 (3): Creating Forms The Right Way
 
+# from django.contrib.auth.models import User
+# from django.shortcuts import render, redirect, get_object_or_404
+# from .forms import NewTopicForm
+# from .models import Board, Topic, Post
+
+
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+
+
+# def board_topics(request, pk):
+#     board = Board.objects.get(pk=pk)
+#     return render(request, 'topics.html', {'board': board})  
+
+
+# # def new_topic(request, pk):
+# #     board = get_object_or_404(Board, pk=pk)
+# #     return render(request, 'new_topic.html', {'board': board})     
+
+
+# def new_topic(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     user = User.objects.first()  # TODO: get the currently logged in user
+#     if request.method == 'POST':
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():
+#             topic = form.save(commit=False)
+#             topic.board = board
+#             topic.starter = user
+#             topic.save()
+#             post = Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=user
+#             )
+#             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+#     else:
+#         form = NewTopicForm()
+#     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+# PART 5 (1): Protecting Views
+
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
@@ -89,11 +135,7 @@ def board_topics(request, pk):
     return render(request, 'topics.html', {'board': board})  
 
 
-# def new_topic(request, pk):
-#     board = get_object_or_404(Board, pk=pk)
-#     return render(request, 'new_topic.html', {'board': board})     
-
-
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in user
