9e86ff07a2a7975751f033e4b299ac57a34b85a2
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 10:08:40 +0700

    Part 2 - Django Template Engine Setup

--- boards/views.py
+++ boards/views.py
@@ -5,18 +5,29 @@
 #     return HttpResponse('Hello, World!')
 
 
-# PART 2: Retrieve and display
+# # PART 2 (1/2): Retrieve and display
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from .models import Board
+
+# def home(request):
+#     boards = Board.objects.all()
+#     boards_names = list()
+
+#     for board in boards:
+#         boards_names.append(board.name)
+
+#     response_html = '<br>'.join(boards_names)
+
+#     return HttpResponse(response_html)    
+
+
+
+# PART 2 (2/2): Retrieve and display
+
+from django.shortcuts import render
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)    
+    return render(request, 'home.html', {'boards': boards})
