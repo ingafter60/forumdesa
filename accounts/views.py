ffa95820bdf85d965e64708a0c275b13c1b6ec46
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 13:25:09 +0700

    Part 4 - 4. Adding the Email Field to the Form

--- accounts/views.py
+++ accounts/views.py
@@ -15,19 +15,38 @@
 #     return render(request, 'signup.html', {'form': form})
 
 
-# # Part 3
+# # # Part 3
+
+# from django.contrib.auth import login as auth_login
+# from django.contrib.auth.forms import UserCreationForm
+# from django.shortcuts import render, redirect
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'signup.html', {'form': form})
+
+
+# # Part 4: import the new form, SignUpForm
 
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
