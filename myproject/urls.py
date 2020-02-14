b49b7503b32bfa492481222efcd4215ad994cafc
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 13:42:16 +0700

    Part 4 - 6. Logout (1)

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,8 +7,29 @@
 # ]
 
 
+# from django.conf.urls import url
+# from django.contrib import admin
+
+# from accounts import views as accounts_views
+# from boards import views
+
+# urlpatterns = [
+#     url(r'^$', views.home, name='home'),
+#     url(r'^signup/$', accounts_views.signup, name='signup'),
+#     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
+#     # BORDS
+#     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+#     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+
+#     # ADMIN
+#     url(r'^admin/', admin.site.urls),
+# ]
+
+
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
@@ -16,11 +37,8 @@ from boards import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
-
-    # BORDS
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-
-    # ADMIN
     url(r'^admin/', admin.site.urls),
 ]
