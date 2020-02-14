93a4e622559b65e6392802602f4e897e16a1a09b
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 09:57:40 +0700

    Part 2 - Fundamentals: Created Board, Topic and Post model

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,22 @@
+# PART 1: Hello world
+# from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse('Hello, World!')
+
+
+# PART 2: Retrieve and display
+
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)    
