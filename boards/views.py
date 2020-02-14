5f90236c39904fa8131ac86fca7767fc540f4a94
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 18:28:06 +0700

    Part 5 - 3. Topic Posts View

--- boards/views.py
+++ boards/views.py
@@ -160,11 +160,51 @@
 
 # PART 5 (2): Accessing the Authenticated User
 
+# from django.contrib.auth.decorators import login_required
+# from django.shortcuts import get_object_or_404, redirect, render
+
+# from .forms import NewTopicForm
+# from .models import Board, Post
+
+
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+
+
+# def board_topics(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     return render(request, 'topics.html', {'board': board})
+
+
+# @login_required
+# def new_topic(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     if request.method == 'POST':
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():
+#             topic = form.save(commit=False)
+#             topic.board = board
+#             topic.starter = request.user
+#             topic.save()
+#             Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=request.user
+#             )
+#             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+#     else:
+#         form = NewTopicForm()
+#     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+# PART 5 (3): Topic Posts View
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import NewTopicForm
-from .models import Board, Post
+from .models import Board, Post, Topic
 
 
 def home(request):
@@ -196,3 +236,9 @@ def new_topic(request, pk):
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -73,6 +73,7 @@ urlpatterns = [
     # BORDS
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
     
     # ADMIN
     url(r'^admin/', admin.site.urls),
