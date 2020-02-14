eb5a45fafe1c55c49c7b027b2a4b3646e5ddfe87
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 10:49:59 +0700

    Part 3 - 1. Using the URLs API

--- boards/views.py
+++ boards/views.py
@@ -25,9 +25,26 @@
 
 # PART 2 (2/2): Retrieve and display
 
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .models import Board
+
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+
+
+# PART 3 (1): Using the URLs API
+
+from django.shortcuts import render, get_object_or_404
 from .models import Board
+
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})    
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -14,5 +14,8 @@ from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+
+    # BORDS
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^admin/', admin.site.urls),
 ]
