91aaac451bb9d98a39fd5562a3df946559354a4b
ing <inyoman_gurnitha@yahoo.com>
2020-02-14 18:14:57 +0700

    Part 5 - 2. Accessing the Authenticated User

--- boards/views.py
+++ boards/views.py
@@ -117,12 +117,54 @@
 
 # PART 5 (1): Protecting Views
 
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, get_object_or_404
+# from django.contrib.auth.models import User
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django.contrib.auth.decorators import login_required
+
+# from .forms import NewTopicForm
+# from .models import Board, Topic, Post
+
+
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+
+
+# def board_topics(request, pk):
+#     board = Board.objects.get(pk=pk)
+#     return render(request, 'topics.html', {'board': board})  
+
+
+# @login_required
+# def new_topic(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
+#     user = User.objects.first()  # TODO: get the currently logged in user
+#     if request.method == 'POST':
+#         form = NewTopicForm(request.POST)
+#         if form.is_valid():
+#             topic = form.save(commit=False)
+#             topic.board = board
+#             topic.starter = user
+#             topic.save()
+#             post = Post.objects.create(
+#                 message=form.cleaned_data.get('message'),
+#                 topic=topic,
+#                 created_by=user
+#             )
+#             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+#     else:
+#         form = NewTopicForm()
+#     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+
+# PART 5 (2): Accessing the Authenticated User
+
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import NewTopicForm
-from .models import Board, Topic, Post
+from .models import Board, Post
 
 
 def home(request):
@@ -131,25 +173,24 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
-    return render(request, 'topics.html', {'board': board})  
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
 
 
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
